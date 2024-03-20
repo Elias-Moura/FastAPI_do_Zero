c70854e54f3454265712d3c2e6dd39908541d823
elias-moura <elias.smourayt@gmail.com>
2024-03-20 14:30:08 -0300

    Feat: Exercício com TDD de retornar um html.

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=200, response_class=HTMLResponse)
+def hw_html():
+    return '<h1>Hello World!</h1>'
--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -12,3 +12,12 @@ def test_hello_world_deve_retornar_200_e_ola_mundo():
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_hw_html_deve_retornar_h1_ola_mundo():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == 200
+    assert response.text == '<h1>Hello World!</h1>'
